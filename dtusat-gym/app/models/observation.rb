0ce7012d21370412f8a2a46f081786c18ae2ba4a
Kasper Bjørn Nielsen <kbn@ipvision.dk>
2010-11-07 22:26:19 +0100

    New position format

--- dtusat-gym/app/models/observation.rb
+++ dtusat-gym/app/models/observation.rb
@@ -1,10 +1,13 @@
 class Observation < ActiveRecord::Base
 
+  # Example: "55 46 37.7 N 12 17 21.4 E"
+  POSITION_FORMAT = /^((\d+\s){2}(\d+\.\d+\s)[a-zA-Z])+\s+((\d+\s){2}(\d+\.\d+\s)[a-zA-Z])$/
+
 	belongs_to :user
 
 	validate :position_format
 	def position_format
-		errors.add :position, 'Formatet for position er "d d d A" hvor d er et decimaltal (med punktum som decimalseparator) og A er et bogstav.' if (self.position =~ /^(\d+\.\d+\s){3}[a-zA-Z]$/).nil?
+		errors.add :position, 'Formatet for position er "h h d A h h d A" hvor h er et heltal, d er et decimaltal (med punktum som decimalseparator) og A er et bogstav. Eksempel: "55 46 37.7 N 12 17 21.4 E"' if (self.position =~ POSITION_FORMAT).nil?
 	end
 
 end
--- dtusat-gym/test/unit/observation_test.rb
+++ dtusat-gym/test/unit/observation_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class ObservationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "the format of position" do
+    assert Observation.new(:position => "55 46 37.7 N 12 17 21.4 E").valid?
+    assert !Observation.new(:position => "55 46 37.7 N").valid?
+    assert !Observation.new(:position => "55 46 37,7 N 12 17 21,4 E").valid?
+    assert !Observation.new(:position => "55.0 46.0 37.7 N 12.0 17.0 21.4 E").valid?
   end
 end
