9768f43dfbbc2338494ea5c524a738b87ce4b3ed
Kasper Bjørn Nielsen <kbn@ipvision.dk>
2010-11-08 16:57:42 +0100

    Position format in user

--- dtusat-gym/app/models/observation.rb
+++ dtusat-gym/app/models/observation.rb
@@ -2,12 +2,14 @@ class Observation < ActiveRecord::Base
 
   # Example: "55 46 37.7 N 12 17 21.4 E"
   POSITION_FORMAT = /^((\d+\s){2}(\d+\.\d+\s)[a-zA-Z])+\s+((\d+\s){2}(\d+\.\d+\s)[a-zA-Z])$/
+  POSITION_MESSAGE = 'Formatet for position er "h h d A h h d A" hvor h er et heltal, d er et decimaltal (med punktum som decimalseparator) og A er et bogstav. Eksempel: "55 46 37.7 N 12 17 21.4 E"'
 
 	belongs_to :user
 
-	validate :position_format
-	def position_format
-		errors.add :position, 'Formatet for position er "h h d A h h d A" hvor h er et heltal, d er et decimaltal (med punktum som decimalseparator) og A er et bogstav. Eksempel: "55 46 37.7 N 12 17 21.4 E"' if (self.position =~ POSITION_FORMAT).nil?
-	end
+  validates_presence_of :user_id, :message => 'Vælg venligst en bruger'
+  validates_presence_of :elevation, :message => 'Indtast venligst en elevation'
+  validates_presence_of :azimuth, :message => 'Indtast venligst en azimuth'
+  validates_presence_of :position, :message => 'Indtast venligst en position'
+	validates_format_of :position, :with => Observation::POSITION_FORMAT, :message => Observation::POSITION_MESSAGE  
 
 end
--- dtusat-gym/app/models/user.rb
+++ dtusat-gym/app/models/user.rb
@@ -2,14 +2,10 @@ class User < ActiveRecord::Base
 
 	validates_presence_of :username
 	validates_uniqueness_of :username
-	validate :position_format
+	validates_format_of :position, :with => Observation::POSITION_FORMAT, :message => Observation::POSITION_MESSAGE
 
 	has_many :observations, :dependent => :destroy
 	has_many :sessions, :dependent => :destroy
-
-	def position_format
-		errors.add :position, 'Formatet for position er "d d d A" hvor d er et decimaltal (med punktum som decimalseparator) og A er et bogstav.' if (self.position =~ /^(\d+\.\d+\s){3}[a-zA-Z]$/).nil?
-	end
 
 	def latest_session
 		self.sessions.find(:first, :order => 'created_at DESC')
